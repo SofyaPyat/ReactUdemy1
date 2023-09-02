44304baa9bc8bb4fbd6f22d2dd65b56175693271
Софья <JesichKA13@yandex.ru>
2023-09-02 13:18:40 +0300

    test

--- script.js
+++ script.js
@@ -1,125 +1,24 @@
 "use strict";
 
-// console.log(document.head);
-// console.log(document.documentElement);
-// console.log(document.documentURI);
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector(".box");
 
-// console.log(document.body.childNodes);
-// console.log(document.body.firstElementChild);
-// console.log(document.body.lastChild);
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
 
-// console.log(document.getElementById("current"));
-// console.log(document.querySelector("#current").parentNode.parentNode);
+        console.log("start");
+        console.log(e.touches);
+    });
 
-// console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == "#text")
-    {
-        continue;
-    }
-    console.log(node);
-}
+        console.log(e.targetTouches[0].pageX);
+    });
 
-function pow (base, state) {
-    if (state === 0)
-        return 1;
-    else 
-        return pow(base, state - 1) * base;
-}
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
 
-console.log(pow(2, 4));
-console.log(pow(13, 3));
-
-console.log(pow(5,0));
-
-
-let students = { 
-    js: [{
-        name: "John",
-        progress: 100
-    },
-    {
-        name: "Ivan",
-        progress: 60
-    }],
-
-    html: {
-        basic: [{
-            name: "Peter",
-            progress: 20
-        },
-        {
-            name: "Ann",
-            progress: 18
-        }],
-    pro: [
-        {
-            name: "Sam",
-            progress: 10
-        }
-    ],
-    semi: {
-        students: [
-            {
-                name: "Test",
-                progress: 100
-            }
-        ]
-    }
-    }
-};
-
-// function getTotalProgressByIteration (data) {
-//     let total = 0;
-//     let students = 0;
-
-//     for (let course of Object.values(data)) {
-//         if (Array.isArray(course)) {
-//             students += course.length;
-
-//             for (let i = 0; i < course.length; i++) {
-//                 total += course[i].progress;
-//             }
-//         }
-//         else {
-//             for (let subCourse of Object.values(course)) {
-//                 students += subCourse.length;
-
-//                 for (let i = 0; i< subCourse.length; i++) {
-//                     total += subCourse[i].progress;
-//                 }
-//             }            
-//         }
-//     }
-//     return total / students;
-// }
-
-function getTotalProgressByRecursion(data) {
-    
-    if (Array.isArray(data)) {
-        let total = 0;
-
-        for (let i = 0; i < data.length; i++) {
-            total += data[i].progress;
-        }
-
-        return [total, data.length];
-    } else {
-        let total = [0,0];
-
-        for (let subData of Object.values(data)) {
-            const subDataArr = getTotalProgressByRecursion(subData);
-
-            total[0] += subDataArr[0];
-            total[1] += subDataArr[1]; 
-        }
-        return total;
-    }
-}
-
-
-// console.log(getTotalProgressByIteration(students));
-
-const res = getTotalProgressByRecursion(students);
-console.log(res[0]/res[1]);
+        console.log("end");
+    });
+})
