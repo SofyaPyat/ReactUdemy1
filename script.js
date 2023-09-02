f8d7c94d9f57b09f4ef9f0ccfa0c0ef4b3a6fc23
Софья <JesichKA13@yandex.ru>
2023-09-02 20:58:48 +0300

    all listeners > per

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector(".box");
-
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-
-        console.log("start");
-        console.log(e.touches);
-    });
-
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-
-        console.log(e.targetTouches[0].pageX);
-    });
-
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
-
-        console.log("end");
-    });
-})
